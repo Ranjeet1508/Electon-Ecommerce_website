91431427ecd40f957a79daf3b09aede96b3e5b1e
Ranjeet1508 <danavrj1998@gmail.com>
2023-05-18 14:51:54 +0530

    Electon

---
+++ products/camera.js
@@ -0,0 +1,44 @@
+const fetchedData = async () => {
+    try{
+        let res = await fetch(`http://localhost:3000/Camera`);
+        let data = await res.json();
+        console.log(data);
+        displayData(data);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+
+const displayData = ((data) => {
+    let mainSection = document.getElementById("myproduct");
+    mainSection.innerHTML = "";
+    
+    data.forEach((elem) => {
+        let div = document.createElement("div");
+        div.setAttribute("id","box");
+
+        let upperDiv = document.createElement("div");
+        upperDiv.setAttribute("id","imagediv")
+        let image = document.createElement("img");
+        image.setAttribute("id","laptop-img")
+        image.src = elem.image;
+        upperDiv.append(image);
+
+        let bottomDiv = document.createElement("div");
+        bottomDiv.setAttribute("id","detail-div")
+        let title = document.createElement("h4");
+        title.setAttribute("class","laptop-title")
+        title.textContent = elem.title;
+        let br = document.createElement("br");
+        let price = document.createElement("h2");
+        price.textContent = "Rs. " + elem.price;
+
+        bottomDiv.append(title,br,price);
+        div.append(upperDiv,bottomDiv);
+        mainSection.append(div);
+    })
+})
+
+
+export {fetchedData,displayData}
