8b7386167edf3e2a1d59e7761d5b78da4b624f29
arvindsp988 <patilarvindsbusi@gmail.com>
2023-05-19 08:27:42 +0530

    footer

---
+++ component/footer.js
@@ -0,0 +1,91 @@
+function footer(){
+    return `
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="footer.css">
+    <title>Footer</title>
+</head>
+<body> -->
+    <div class="footer_total">
+        <div class="footer_upper">
+            <div class="white_box">
+                <div class="Subscribe">
+                        <h2>Subscribe Newsletter</h2>
+                </div>
+                <div class="place"> <input class="placeholder" type="text" placeholder="_Email adress">
+                </div>
+                
+                <div class="care">
+                    <!-- <div> <img class="headphone" src="https://cdn-icons-png.flaticon.com/512/27/27130.png"/></div> -->
+                    <div class="contact">
+                       <p>Call us 24/7:<br>(+62) 0123 567 789</p> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer_lower_part">
+            <div class="column1">
+                <div class="logo1">
+
+                    <!-- <img src="https://bizzy.ams3.digitaloceanspaces.com/prod/companies/be/logos/fa6fd26d-f23a-416a-822e-624a0eca6e3b/100075832276249.png"/> -->
+                </div>
+                <div class="adress">
+                    <h5>64 st james boulevard<br>
+                        hoswick, ze2 7zj</h5>
+                        <hr>
+                </div>
+                <div class="logo">
+                    <p>G</p>
+                    <p>F</p>
+                    <p>W</p>
+                    <!-- <img src="https://www.nicepng.com/png/detail/13-134988_google-logo-png-transparent-google-g-logo-black.png"/>
+                    <img src="https://w7.pngwing.com/pngs/624/811/png-transparent-computer-icons-facebook-social-media-f-logo-cross-brand.png"/>
+                    <img src="https://png.pngtree.com/element_our/png/20181011/whatsapp-social-media-icon-design-template-vector-png_127058.jpg"/> -->
+                </div>
+            </div>
+            <div class="column2">
+                
+                <h4>Find product</h4>
+                <p>Brownze arnold</p>
+                <p>Chronograph blue</p>
+                <p>Smart phones</p>
+                <p>Automatic watch</p>
+                <p>Hair straightners</p>
+                <!-- <ul class="column2_list">
+                    <li>Brownze arnold</li>
+                    <li>Chronograph blue</li>
+                    <li>Smart phones</li>
+                    <li>Automatic watch</li>
+                    <li>Hair straightners</li>
+                </ul> -->
+                
+            </div>
+            <div class="column3">
+                <h4>Get help</h4>
+                <p>About us</p>
+                <p>Contact us</p>
+                <p>Return policy</p>
+                <p>Privacy policy</p>
+                <p>Payment policy</p>
+            </div>
+            <div class="column4">
+                <h4>About us</h4>
+                <p>News</p>
+                <p>Service</p>
+                <p>Our policy</p>
+                <p>Customer care</p>
+                <p>Faq's</p>
+            </div>
+        </div>
+    </div>    
+<!-- </body>
+</html> -->
+`}
+
+export default navbar;
